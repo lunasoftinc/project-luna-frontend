3fa592f1b18ffa386e7ec463a106d80cdb9d65dd
Mauricio Leite <mauricioleite1@gmail.com>
2024-06-12 12:28:02 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>Luna</main>
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>Luna Page</main>
 	);
 }
